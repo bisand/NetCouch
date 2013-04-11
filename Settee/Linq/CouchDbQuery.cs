26e983b58f00610d6f32eb5a5345bb7b34d9a130
André Biseth <andre@biseth.net>
2013-04-12 00:32:52 +0200

    Starting the work on Linq provider

---
+++ Settee/Linq/CouchDbQuery.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Biseth.Net.Settee.Linq
+{
+    public class CouchDbQuery<T> : IOrderedQueryable<T>
+    {
+        public CouchDbQuery(IQueryProvider provider, Expression expression)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider");
+            }
+
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+
+            if (!typeof (IQueryable<T>).IsAssignableFrom(expression.Type))
+            {
+                throw new ArgumentOutOfRangeException("expression");
+            }
+
+            Provider = provider;
+            Expression = expression;
+        }
+
+        public CouchDbQuery()
+        {
+            Expression = Expression.Constant(this);
+            Provider = new CouchDbQueryProvider();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (Provider.Execute<IEnumerable<T>>(Expression)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (Provider.Execute<IEnumerable>(Expression)).GetEnumerator();
+        }
+
+        public Expression Expression { get; private set; }
+
+        public Type ElementType
+        {
+            get { return typeof (T); }
+        }
+
+        public IQueryProvider Provider { get; private set; }
+    }
+}
---
+++ Settee/Linq/CouchDbQueryProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Biseth.Net.Settee.Linq
+{
+    public class CouchDbQueryProvider : IQueryProvider
+    {
+        public IQueryable CreateQuery(Expression expression)
+        {
+            var elementType = TypeSystem.GetElementType(expression.Type);
+            try
+            {
+                return (IQueryable) Activator.CreateInstance(typeof (CouchDbQuery<>).MakeGenericType(elementType), new object[] {this, expression});
+            }
+            catch (System.Reflection.TargetInvocationException tie)
+            {
+                throw tie.InnerException;
+            }
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new CouchDbQuery<TElement>(this, expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            return TerraServerQueryContext.Execute(expression, false);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            bool IsEnumerable = (typeof(TResult).Name == "IEnumerable`1");
+
+            return (TResult)TerraServerQueryContext.Execute(expression, IsEnumerable);
+        }
+    }
+
+    public class TerraServerQueryContext
+    {
+        public static object Execute(Expression expression, bool b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
