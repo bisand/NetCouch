097f53f136d2616f6c5c39a5a3609d66532c073b
André Biseth <andre@biseth.net>
2013-04-19 16:28:23 +0200

    Building queries

--- Settee/Linq/CouchDbViewQueryBuilder.cs
+++ Settee/Linq/CouchDbViewQueryBuilder.cs
@@ -1,77 +1,22 @@
-﻿using System.Text;
-using Biseth.Net.Settee.CouchDb.Api;
-using Biseth.Net.Settee.CouchDb.Api.Extensions;
-using Biseth.Net.Settee.Models.Couch.DesignDoc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Linq.Expressions;
-using System.Net;
+using System.Text;
 
 namespace Biseth.Net.Settee.Linq
 {
-    public class CouchDbQueryProvider<T> : QueryProvider
-    {
-        private readonly ICouchApi _couchApi;
-
-        public CouchDbQueryProvider(ICouchApi couchApi)
-        {
-            _couchApi = couchApi;
-        }
-
-        public override string GetQueryText(Expression expression)
-        {
-            return Translate(expression).QueryText;
-        }
-
-        public override object Execute(Expression expression)
-        {
-            var result = Translate(expression);
-            new CouchDbViewBuilder().Build(result);
-            new CouchDbQueryBuilder().Build(result);
-
-            // p => (p.Make == "Saab" && p.Model == "1337" || p.Make != "Volvo" && p.Model != "2013")
-            // The not equal operators should be put into an index. If a combined statement produce an empty query,
-            // we probably would like to do another query with no keys...
-
-            var queryString = "keys=" + Uri.EscapeDataString("[[\"Saab\",\"1337\"]]") + "&include_docs=true";
-            var headResult = _couchApi.Root().Db(_couchApi.DefaultDatabase).DesignDoc(result.DesignDocName).View(result.ViewName, queryString).Head();
-            if (headResult != null && headResult.StatusCode == HttpStatusCode.NotFound)
-            {
-                //CreateView
-            }
-            var queryResult = _couchApi.Root().Db(_couchApi.DefaultDatabase).DesignDoc(result.DesignDocName).View(result.ViewName, queryString).Get<ViewResponse<T>>();
-
-            var elementType = TypeSystem.GetElementType(expression.Type);
-            return (queryResult.DataDeserialized.Rows ?? new List<ViewRow<T>>()).Select(x => x.Doc);
-            //return Activator.CreateInstance(
-            //    typeof (ProjectionReader<>).MakeGenericType(elementType),
-            //    BindingFlags.Instance | BindingFlags.NonPublic, null,
-            //    new object[] {reader, projector},
-            //    null
-            //    );
-        }
-
-        private static TranslateResult Translate(Expression expression)
-        {
-            expression = Evaluator.PartialEval(expression);
-            var proj = (ProjectionExpression)new QueryBinder().Bind(expression);
-            var result = new QueryFormatter().Format(proj.Source);
-            return result;
-        }
-    }
-
-    public class CouchDbViewBuilder
+    public class CouchDbViewQueryBuilder
     {
         private readonly StringBuilder _view;
+        private readonly StringBuilder _query;
 
-        public CouchDbViewBuilder()
+        public CouchDbViewQueryBuilder()
         {
+            _query = new StringBuilder();
             _view = new StringBuilder();
             _view.Append("function(doc) { if (doc.doc_type && doc.doc_type == '");
         }
 
-        public string Build(TranslateResult result)
+        public ViewQuery Build(TranslateResult result)
         {
             var notEquals = result.Statements.Where(x => x.NodeType == ExpressionType.NotEqual).ToList();
             var equals = result.Statements.Where(x => x.NodeType == ExpressionType.Equal).ToList();
@@ -108,20 +53,31 @@ namespace Biseth.Net.Settee.Linq
                 var statements = @equals.Where(x => x.Level == equalGroup.Key).ToList();
                 if (statements.Count > 1)
                 {
+                    _query.Append("keys=[");
                     _view.Append("emit([");
                     foreach (var statement in statements)
                     {
                         if (statement.Right is ConstantExpression && statement.Left is ColumnExpression)
                         {
                             _view.Append("doc." + (statement.Left as ColumnExpression).Name + ",");
+                            if ((statement.Right as ConstantExpression).Value is string)
+                                _query.Append("\"" + (statement.Right as ConstantExpression).Value + "\",");
+                            else
+                                _query.Append("" + (statement.Right as ConstantExpression).Value + ",");
                         }
                         else if (statement.Left is ConstantExpression && statement.Right is ColumnExpression)
                         {
                             _view.Append("doc." + (statement.Right as ColumnExpression).Name + ",");
+                            if ((statement.Left as ConstantExpression).Value is string)
+                                _query.Append("\"" + (statement.Left as ConstantExpression).Value + "\",");
+                            else
+                                _query.Append("" + (statement.Left as ConstantExpression).Value + ",");
                         }
                     }
-                    _view.Remove(_view.Length-1, 1);
+                    _view.Remove(_view.Length - 1, 1);
+                    _query.Remove(_query.Length -2, 2);
                     _view.Append("],null);");
+                    _query.Append("]]");
                 }
                 else if (statements.Count > 0)
                 {
@@ -145,7 +101,13 @@ namespace Biseth.Net.Settee.Linq
             _view.Append(" } ");
             _view.Append(" }");
 
-            return _view.ToString();
+            return new ViewQuery {View = _view.ToString(), Query = _query.ToString()};
         }
+    }
+
+    public class ViewQuery
+    {
+        public string View { get; set; }
+        public string Query { get; set; }
     }
 }
