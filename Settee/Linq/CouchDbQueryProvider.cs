20b6c4168f64ce2115083fe305be61f6a4115db9
André Biseth <andre@biseth.net>
2013-04-24 19:54:45 +0200

    Cleaning up query provider.

--- Settee/Linq/CouchDbQueryProvider.cs
+++ Settee/Linq/CouchDbQueryProvider.cs
@@ -28,30 +28,58 @@ namespace Biseth.Net.Settee.Linq
             var result = Translate(expression);
             var viewQuery = new CouchDbViewQueryBuilder().Build(result);
 
-            // p => (p.Make == "Saab" && p.Model == "1337" || p.Make != "Volvo" && p.Model != "2013")
-            // The not equal operators should be put into an index. If a combined statement produce an empty query,
-            // we probably would like to do another query with no keys...
-
-            var queryString = viewQuery.Query; //"keys=" + Uri.EscapeDataString("[[\"Saab\",\"1337\"]]") + "&include_docs=true";
-            var headResult = _couchApi.Root().Db(_couchApi.DefaultDatabase).DesignDoc(result.DesignDocName).View(result.ViewName, queryString).Head();
-            if (headResult != null && headResult.StatusCode == HttpStatusCode.NotFound)
-            {
-                var designDocResult = _couchApi.Root().Db(_couchApi.DefaultDatabase).DesignDoc(result.DesignDocName).Get<DesignDoc>();
-                var designDoc = designDocResult.DataDeserialized;
-                designDoc.Views[result.ViewName] = new View{Map = viewQuery.View};
-                var responseData = _couchApi.Root().Db(_couchApi.DefaultDatabase).DesignDoc(result.DesignDocName).Put<DesignDoc, object>(designDoc);
-            }
+            var queryString = viewQuery.Query;
             queryString += "&include_docs=true";
-            var queryResult = _couchApi.Root().Db(_couchApi.DefaultDatabase).DesignDoc(result.DesignDocName).View(result.ViewName, queryString).Get<ViewResponse<T>>();
 
-            var elementType = TypeSystem.GetElementType(expression.Type);
-            return (queryResult.DataDeserialized.Rows ?? new List<ViewRow<T>>()).Select(x => x.Doc);
-            //return Activator.CreateInstance(
-            //    typeof (ProjectionReader<>).MakeGenericType(elementType),
-            //    BindingFlags.Instance | BindingFlags.NonPublic, null,
-            //    new object[] {reader, projector},
-            //    null
-            //    );
+            // Query the database.
+            var queryResult =
+                _couchApi.Root()
+                         .Db(_couchApi.DefaultDatabase)
+                         .DesignDoc(result.DesignDocName)
+                         .View(result.ViewName, queryString)
+                         .Get<ViewResponse<T>>();
+
+
+            // Create the view if it doess not exist.
+            if (queryResult != null && queryResult.StatusCode == HttpStatusCode.NotFound)
+            {
+                // Retrieve the current design doc.
+                var designDocResult =
+                    _couchApi.Root()
+                             .Db(_couchApi.DefaultDatabase)
+                             .DesignDoc(result.DesignDocName)
+                             .Get<DesignDoc>();
+
+                // Assign the view to the design doc object
+                var designDoc = designDocResult.DataDeserialized;                
+                designDoc.Views[result.ViewName] = new View{Map = viewQuery.View};
+
+                // Save the design doc back to the server.
+                var responseData =
+                    _couchApi.Root()
+                             .Db(_couchApi.DefaultDatabase)
+                             .DesignDoc(result.DesignDocName)
+                             .Put<DesignDoc, object>(designDoc);
+
+                // If the design doc was successfully created, we re-run the query.
+                if (responseData != null && responseData.StatusCode == HttpStatusCode.Created)
+                {
+                    queryResult =
+                        _couchApi.Root()
+                                 .Db(_couchApi.DefaultDatabase)
+                                 .DesignDoc(result.DesignDocName)
+                                 .View(result.ViewName, queryString)
+                                 .Get<ViewResponse<T>>();
+                }
+            }
+
+
+            // Try to extract the result.
+            if (queryResult != null)
+                return (queryResult.DataDeserialized.Rows ?? new List<ViewRow<T>>()).Select(x => x.Doc);
+            
+            // Something bad happened. We just return an empty result.
+            return new List<ViewRow<T>>();
         }
 
         private static TranslateResult Translate(Expression expression)
--- SetteeTests/LinqTests.cs
+++ SetteeTests/LinqTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using Biseth.Net.Settee.CouchDb.Api;
 using Biseth.Net.Settee.Http;
 using Biseth.Net.Settee.Linq;
@@ -17,8 +19,18 @@ namespace SetteeTests
 
             var query = new CouchDbQuery<Car>(new CouchDbQueryProvider<Car>(api));
             //var cars = query.Where(p => (((p.Make == "Saab" || (p.Model == "1337" && p.HorsePowers == 200)) || p.Make != "Volvo") && p.Model != "2013")).ToList();
-            var cars = query.Where(p => (p.Make == "Saab" && (p.Model == "1337" || p.HorsePowers == 1337))).ToList();
-            Assert.That(cars != null);
+            //var cars = query.Where(p => (p.Make == "Saab" && (p.Model == "1337" || p.HorsePowers == 1337))).ToList();
+            Console.WriteLine("Starting to process queries...");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                var cars = query.Where(p => p.Model == "1337").ToList();
+                Assert.That(cars != null && cars.Count > 0);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Finished!");
+            Console.WriteLine("Elapsed: {0}", stopwatch.ElapsedMilliseconds);
         }
     }
 }
