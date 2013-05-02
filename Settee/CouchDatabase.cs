c20a83e00541f49bce8552173299a415f7fb1f80
André Biseth <andre@biseth.net>
2013-05-02 10:16:03 +0200

    Adding database and session class

---
+++ Settee/CouchDatabase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using Biseth.Net.Settee.CouchDb.Api;
+using Biseth.Net.Settee.Http;
+
+namespace Biseth.Net.Settee
+{
+    public class CouchDatabase : IDisposable
+    {
+        private bool _disposed;
+        private readonly Component _component = new Component();
+        private readonly RequestClient _client;
+        private CouchApi _api;
+
+        public CouchDatabase(string serverUrl)
+        {
+            _client = new RequestClient(serverUrl);
+        }
+
+        public CouchDbSession OpenSession(string databaseName)
+        {
+            _api = new CouchApi(_client, databaseName);
+            var session = new CouchDbSession(_api);
+            return session;
+        }
+        #region IDisposable members
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+            if (disposing)
+                _component.Dispose();
+
+            _disposed = true;
+        }
+
+        ~CouchDatabase()
+        {
+            Dispose(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Settee/CouchDbSession.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using Biseth.Net.Settee.CouchDb.Api;
+using Biseth.Net.Settee.Http;
+using Biseth.Net.Settee.Linq;
+
+namespace Biseth.Net.Settee
+{
+    public class CouchDbSession : IDisposable
+    {
+        private readonly ICouchApi _api;
+        private readonly Component _component = new Component();
+        private bool _disposed;
+
+        public CouchDbSession(ICouchApi api)
+        {
+            _api = api;
+        }
+
+        public CouchDbQuery<T> Query<T>()
+        {
+            var query = new CouchDbQuery<T>(new CouchDbQueryProvider<T>(_api));
+            return query;
+        }
+
+        #region IDisposable members
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+            if (disposing)
+                _component.Dispose();
+
+            _disposed = true;
+        }
+
+        ~CouchDbSession()
+        {
+            Dispose(false);
+        }
+
+        #endregion
+    }
+}
