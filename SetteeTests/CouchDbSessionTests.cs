04c62f4d1df5e6e999fe8a4ed877998724b48eb4
André Biseth <andre@biseth.net>
2013-05-16 18:59:06 +0200

    Changing test

--- SetteeTests/CouchDbSessionTests.cs
+++ SetteeTests/CouchDbSessionTests.cs
@@ -14,7 +14,7 @@ namespace SetteeTests
             {
                 using (var session = database.OpenSession("trivial"))
                 {
-                    var queryable = session.Query<Car>().Where(x => x.HorsePowers == 1337);
+                    var queryable = session.Query<Car>().Where(x => (x.HorsePowers == 1337 || x.Model == "1337") && x.Make == "Saab");
                     var cars = queryable.ToList();
                     Assert.That(cars != null && cars.Count > 0);
                 }
