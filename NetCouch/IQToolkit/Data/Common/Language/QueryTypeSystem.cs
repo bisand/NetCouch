46578694a3364e2baedfec67908ad15cbbec6e25
André Biseth <andre.biseth@pareto.no>
2013-11-25 12:12:44 +0100

    Adding IQToolkit to try to improve linq queries

---
+++ NetCouch/IQToolkit/Data/Common/Language/QueryTypeSystem.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// This source code is made available under the terms of the Microsoft Public License (MS-PL)
+
+using System;
+
+namespace Biseth.Net.Couch.IQToolkit.Data.Common.Language
+{
+    public abstract class QueryType
+    {
+        public abstract bool NotNull { get; }
+        public abstract int Length { get; }
+        public abstract short Precision { get; }
+        public abstract short Scale { get; }
+    }
+
+    public abstract class QueryTypeSystem 
+    {
+        public abstract QueryType Parse(string typeDeclaration);
+        public abstract QueryType GetColumnType(Type type);
+        public abstract string GetVariableDeclaration(QueryType type, bool suppressSize);
+    }
+}
